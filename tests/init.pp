314cd20a6638917ffab86b02410615e234bccb7b
Carl Caum <carl@carlcaum.com>
2011-09-13 17:29:51 +0000

    Don't have eroneous stuff in the test
    I had an EC2 host entry for the stom_server parameter.

--- tests/init.pp
+++ tests/init.pp
@@ -2,7 +2,6 @@ class { 'mcollective':
    mc_security_psk => 'abc123',
    client          => true,
    stomp_port      => 6163,
-   #stomp_server    => 'ec2-107-20-16-247.compute-1.amazonaws.com',
    manage_packages => true,
 }
 
